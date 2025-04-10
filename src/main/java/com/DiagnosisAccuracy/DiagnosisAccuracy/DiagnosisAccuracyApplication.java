f08825b1800db962c5157e9d17f8818b7c321217
Shiana Wilson <swi2112@wgu.edu>
2025-04-10 12:00:08 -0400

    adding empty spring project to repository

---
+++ src/main/java/com/DiagnosisAccuracy/DiagnosisAccuracy/DiagnosisAccuracyApplication.java
@@ -0,0 +1,13 @@
+package com.DiagnosisAccuracy.DiagnosisAccuracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisAccuracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosisAccuracyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DiagnosisAccuracy/DiagnosisAccuracy/DiagnosisAccuracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DiagnosisAccuracy.DiagnosisAccuracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisAccuracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
